273ae0a99fb26d6f8cf186e450993037e36d02fd
Yevhen Zhovtonoh <yevhen.zhovtonoh@kenshoo.com>
2022-02-09 17:01:41 +0200

    Initial commit

--- games/src/test/java/com/skai/games/GameRecordStatsServiceImplTest.java
+++ games/src/test/java/com/skai/games/GameRecordStatsServiceImplTest.java
@@ -1,11 +1,7 @@
 package com.skai.games;
 
-import org.junit.Test;
-
 import java.util.Collection;
 import java.util.List;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class GameRecordStatsServiceImplTest {
 
@@ -17,29 +13,25 @@ public class GameRecordStatsServiceImplTest {
             new GameRecord("Boston Bruins", "Pastrniak", 5, 1)
     );
 
-    private final GameStatsService gameStatsService = new GameStatsServiceImpl();
+        private final Collection<GameRecord> gameRecords2 = List.of(
+            new GameRecord("New York Rangers", "Zibanezhad", 2, 1),
+            new GameRecord("New York Rangers", "Zibanezhad", 2, 1),
+            new GameRecord("Boston Bruins", "Zibanezhad", 5, 1),
+            new GameRecord("Boston Bruins", "Pastrniak", 5, 1)
+    );
 
-    @Test
+    private final Collection<GameRecord> gameRecords3 = List.of(
+            new GameRecord("New York Rangers", "Zibanezhad", 2, 1),
+            new GameRecord("Boston Bruins", "Marchan", 5, 1),
+            new GameRecord("Boston Bruins", "Pastrniak", 5, 1)
+    );
+
     public void winner() {
-        assertThat(gameStatsService.winner(gameRecords), is("Boston Bruins"));
+
     }
 
-    @Test
     public void mvp() {
-        assertThat(gameStatsService.mvp(gameRecords), is("Panarin"));
     }
 }
 
 
-//    private final Collection<GameRecord> gameRecords = List.of(
-//            new GameRecord("New York Rangers", "Zibanezhad", 2, 1),
-//            new GameRecord("New York Rangers", "Zibanezhad", 2, 1),
-//            new GameRecord("Boston Bruins", "Zibanezhad", 5, 1),
-//            new GameRecord("Boston Bruins", "Pastrniak", 5, 1)
-//    );
-//
-//    private final Collection<GameRecord> gameRecords = List.of(
-//            new GameRecord("New York Rangers", "Zibanezhad", 2, 1),
-//            new GameRecord("Boston Bruins", "Marchan", 5, 1),
-//            new GameRecord("Boston Bruins", "Pastrniak", 5, 1)
-//    );
