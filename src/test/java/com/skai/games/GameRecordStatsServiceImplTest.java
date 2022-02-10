50afa0ad6ea2e23ae8e9a53c90958c801bdec6fc
Yevhen Zhovtonoh <yevhen.zhovtonoh@kenshoo.com>
2022-02-10 15:19:42 +0200

    change test data

--- src/test/java/com/skai/games/GameRecordStatsServiceImplTest.java
+++ src/test/java/com/skai/games/GameRecordStatsServiceImplTest.java
@@ -19,13 +19,13 @@ public class GameRecordStatsServiceImplTest {
             new GameRecord("New York Rangers", "Zibanezhad", 2, 1),
             new GameRecord("New York Rangers", "Zibanezhad", 2, 1),
             new GameRecord("Boston Bruins", "Zibanezhad", 5, 1),
-            new GameRecord("Boston Bruins", "Pastrniak", 5, 1)
+            new GameRecord("Boston Bruins", "Pastrniak", 6, 1)
     );
 
     private final Collection<GameRecord> gameRecords3 = List.of(
             new GameRecord("New York Rangers", "Zibanezhad", 2, 1),
             new GameRecord("Boston Bruins", "Marchan", 5, 1),
-            new GameRecord("Boston Bruins", "Pastrniak", 5, 1)
+            new GameRecord("Boston Bruins", "Pastrniak", 5, 2)
     );
 
     public void winner() {
